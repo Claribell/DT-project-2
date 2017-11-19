1cb9549c7d401e579e06af026ed9ccc98b55e55e
Claribell <clari.j12@gmail.com>
2017-11-19 12:06:12 +0530

    blog module added

---
+++ SocialNetworkBackend1/src/main/java/com/niit/SocialNetworkBackend1/model/Blog.java
@@ -0,0 +1,68 @@
+package com.niit.SocialNetworkBackend1.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Blog {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int blogId;
+	private String blogName;
+	private String userName;
+	private String blogContent;
+	Date creationDate;
+	private String status;
+	private int likes;
+	public int getBlogId() {
+		return blogId;
+	}
+	public void setBlogId(int blogId) {
+		this.blogId = blogId;
+	}
+	public String getBlogName() {
+		return blogName;
+	}
+	public void setBlogName(String blogName) {
+		this.blogName = blogName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getBlogContent() {
+		return blogContent;
+	}
+	public void setBlogContent(String blogContent) {
+		this.blogContent = blogContent;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	
+	
+
+}

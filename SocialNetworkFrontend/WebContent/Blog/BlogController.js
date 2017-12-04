a19fe86be48ddb8418b37f0c99b8261058b3c629
Claribell <clari.j12@gmail.com>
2017-12-05 00:48:26 +0530

    added blogcontroller in frontend

--- SocialNetworkFrontend/WebContent/Blog/BlogController.js
+++ SocialNetworkFrontend/WebContent/Blog/BlogController.js
@@ -1,8 +1,8 @@
-myApp.controller('BlogController',function($scope,$http)
+app.controller('BlogController',function($scope,$http)
 		
 {
 	console.log("Http Service called");
-	$http.get("http://localhost:7071/SocialNetworkAppRest1/getAllBlogs")
+	$http.get("http://localhost:7072/SocialNetworkAppRest1/getAllBlogs")
 	     .then(function(response){
 		$scope.blogdata=response.data;
 		console.log("Data stored in scope object");
